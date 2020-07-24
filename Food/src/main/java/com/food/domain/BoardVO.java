3af256a9bd2ced1c5a2f30d78001ef5db9b4506d
ryu <grafeisen.0@gmail.com>
2020-07-24 13:05:47 +0900

    게시판 통합 추가

---
+++ Food/src/main/java/com/food/domain/BoardVO.java
@@ -0,0 +1,134 @@
+package com.food.domain;
+
+public class BoardVO {
+	private String boardType;
+    private int b_no;
+    private String title;
+    private String userId;
+    private String b_content;
+    private String b_date;
+    private int viewCount;
+    private String f_addr1;
+    private String f_addr2;
+    private int f_membercnt;
+    private String f_date;
+    private int f_port;
+    private int f_joincnt;
+    private String d_suspect;
+    private String r_date;
+    private String r_visit_date;
+    private int r_ppl_cnt;
+    private String r_menu;
+	public String getBoardType() {
+		return boardType;
+	}
+	public void setBoardType(String boardType) {
+		this.boardType = boardType;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public int getViewCount() {
+		return viewCount;
+	}
+	public void setViewCount(int viewCount) {
+		this.viewCount = viewCount;
+	}
+	public String getF_addr1() {
+		return f_addr1;
+	}
+	public void setF_addr1(String f_addr1) {
+		this.f_addr1 = f_addr1;
+	}
+	public String getF_addr2() {
+		return f_addr2;
+	}
+	public void setF_addr2(String f_addr2) {
+		this.f_addr2 = f_addr2;
+	}
+	public int getF_membercnt() {
+		return f_membercnt;
+	}
+	public void setF_membercnt(int f_membercnt) {
+		this.f_membercnt = f_membercnt;
+	}
+	public String getF_date() {
+		return f_date;
+	}
+	public void setF_date(String f_date) {
+		this.f_date = f_date;
+	}
+	public int getF_port() {
+		return f_port;
+	}
+	public void setF_port(int f_port) {
+		this.f_port = f_port;
+	}
+	public int getF_joincnt() {
+		return f_joincnt;
+	}
+	public void setF_joincnt(int f_joincnt) {
+		this.f_joincnt = f_joincnt;
+	}
+	public String getD_suspect() {
+		return d_suspect;
+	}
+	public void setD_suspect(String d_suspect) {
+		this.d_suspect = d_suspect;
+	}
+	public String getR_date() {
+		return r_date;
+	}
+	public void setR_date(String r_date) {
+		this.r_date = r_date;
+	}
+	public String getR_visit_date() {
+		return r_visit_date;
+	}
+	public void setR_visit_date(String r_visit_date) {
+		this.r_visit_date = r_visit_date;
+	}
+	public int getR_ppl_cnt() {
+		return r_ppl_cnt;
+	}
+	public void setR_ppl_cnt(int r_ppl_cnt) {
+		this.r_ppl_cnt = r_ppl_cnt;
+	}
+	public String getR_menu() {
+		return r_menu;
+	}
+	public void setR_menu(String r_menu) {
+		this.r_menu = r_menu;
+	}
+	public int getB_no() {
+		return b_no;
+	}
+	public void setB_no(int b_no) {
+		this.b_no = b_no;
+	}
+	public String getB_content() {
+		return b_content;
+	}
+	public void setB_content(String b_content) {
+		this.b_content = b_content;
+	}
+	public String getB_date() {
+		return b_date;
+	}
+	public void setB_date(String b_date) {
+		this.b_date = b_date;
+	}
+    
+    
+    
+}
