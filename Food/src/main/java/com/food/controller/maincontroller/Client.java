7f24881c241b35ca927744dd851f9085af279e39
maria <rkddpdms12@naver.com>
2020-08-13 20:11:18 +0900

    fd

--- Food/src/main/java/com/food/controller/maincontroller/Client.java
+++ Food/src/main/java/com/food/controller/maincontroller/Client.java
@@ -2,6 +2,8 @@ package com.food.controller.maincontroller;
 
 import java.net.Socket;
 
+import com.sun.mail.iap.ByteArray;
+
 
 public class Client {
 	Socket socket = null;
@@ -17,6 +19,17 @@ public class Client {
 	String ip;
 	String filesize;
 	String search;
+	String resultimage;
+	String resize;
+	
+
+	public String getResize() {
+		return resize;
+	}
+
+	public String getResultimage() {
+		return resultimage;
+	}
 
 	public Client(String resultweather,String resulttemp,String region,String most,String search,String fileName,String what) {
 		this.resultweather = resultweather;
@@ -37,6 +50,8 @@ public class Client {
 			Sender.start();
 			Sender.join();
 			result = Sender.getResult();
+			resultimage = Sender.getResultimage();
+			resize = Sender.getResize();
 			ip = Sender.getIp();
 			filesize = Sender.getFilesize();
 			
