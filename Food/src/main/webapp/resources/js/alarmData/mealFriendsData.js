0488d5d9934a2d93c7bae9b93809f983b6ceb1d7
kangdada <ektmf7778@gmail.com>
2020-08-14 10:40:18 +0900

    밥친구게시판 수정

--- Food/src/main/webapp/resources/js/alarmData/mealFriendsData.js
+++ Food/src/main/webapp/resources/js/alarmData/mealFriendsData.js
@@ -6,16 +6,9 @@
 $('.mary').on('click',function(){
 	var b_no=$(this).parent().parent().children().find('a').eq(0).text();
 	var writer=$(this).parent().parent().children().eq(3).text();
-
 	var joinuser=$(this).parent().parent().children().eq(4).find('input[type=hidden][name=joinId]').val();
 	alert(joinuser);
-
-	console.log('?title='+$(this).parent().parent().children().find('a').eq(1).text()+'&userId='+$('#sessionId').val()+'&f_userId='+$(this).parent().parent().children().eq(4).find('input[type=hidden][name=joinId]').val()+"&b_no="+b_no);
-	window.open('http://192.168.0.17:8080/Food/mealBoard/chatBox.do?title='+$(this).parent().parent().children().find('a').eq(1).text()+'&userId='+$('#sessionId').val()+'&f_userId='+$(this).parent().parent().children().eq(4).find('input[type=hidden][name=joinId]').val()+"&b_no="+b_no,'_blank','width=502,height=720,left=500,top=100,location=no,status=no');
-
-
-	window.open('http://192.168.0.17:8080/Food/mealBoard/chatBox.do?title='+$(this).parent().parent().children().find('a').eq(1).text()+'&userId='+$('#sessionId').val(),'_blank','width=502,height=720,left=500,top=100,location=no,status=no');
-
+	window.open('http://https://115.91.88.227:60000/Food/mealBoard/chatBox.do?title='+$(this).parent().parent().children().find('a').eq(1).text()+'&userId='+$('#sessionId').val()+'&f_userId='+$(this).parent().parent().children().eq(4).find('input[type=hidden][name=joinId]').val()+"&b_no="+b_no,'_blank','width=502,height=720,left=500,top=100,location=no,status=no');
 	
 	
 	$.ajax({
--- Food/src/main/webapp/resources/js/join.js
+++ Food/src/main/webapp/resources/js/join.js
@@ -15,58 +15,8 @@
 			//참가중 문구 지우기
 			$("#join").remove();
 		}
-	} else {
-		
-	}
+	} 
 	
-	//참가하기 버튼을 눌렀을때 
-	$("#joinBtn").click(function(){
-		
-	        $.ajax({
-	        		type:'post',
-	        		async:true,
-	                url : 'mealjoin.do',
-	                contentType :'application/x-www-form-urlencoded;charset=UTF-8',
-	                data :
-	            		"f_userId=" + $("#joinid").val()+","+$("#id").val()+
-	            		"&b_no="+ $("#b_no").val(),
-	            	dataType:"text",
-	                success : function(resultData){
-	                	alert("밥친구로 확정되었습니다.");
-	                	//넘겨받은 결과값이 0이 아니라면 
-	                	if(resultData != 0){
-	                		//참가하기 버튼 숨기고
-	                		$("#joinBtn").css("display", "none");
-	                		//취소하기버튼 띄워주기 
-	                		var $div = $('<input class="btn btn-danger" type="button" id="cancel" value="취소하기">');
-	                		$("#joincnt").append($div);
-	                		
-	                		//span태그 안에 마감되었습니다 라는 문자가 있다면 
-	                		if(end.match("마감되었습니다.")){
-	                			//취소버튼 지워주기
-	                			$("#cancel").remove();
-	                		}
-	                		
-	                	}else{
-	                		
-	                	}
-	                	
-	                	
-	                },
-	        		error:function(xhr, ajaxOptions, thrownError){
-	        			alert("에러"+xhr.status);
-	        			alert("에러"+thrownError);
-	        			console.log(e);
-	        		}
-	        });
-		});
-	
-	
-		
-		
-		
-
-		
 	
 	window.history.forward();
 	 function noBack(){window.history.forward();}
