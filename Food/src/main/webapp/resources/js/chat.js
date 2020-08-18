c1e1eeff001a4e86ef97a72a1a00e73c6fd2621c
kangdada <ektmf7778@gmail.com>
2020-08-18 16:57:02 +0900

    채팅방 버튼 수정

--- Food/src/main/webapp/resources/js/chat.js
+++ Food/src/main/webapp/resources/js/chat.js
@@ -36,7 +36,7 @@
 
 	//해당하는 url로 접속을하면 websocket으로 통로를 따로 열어준다.
 	var webSocket = new WebSocket('wss://115.91.88.227:60000/Food/mealBoard/chatBox.do');
-   // var webSocket = new WebSocket('ws://192.168.0.17:8080/Food/mealBoard/chatBox.do');
+    //var webSocket = new WebSocket('ws://192.168.0.17:8080/Food/mealBoard/chatBox.do');
     var inputMessage = document.getElementById('messagearea');
     
     
@@ -58,94 +58,50 @@
 		$('#messagearea').val('')
 	});
  
-    
-    //참여하기를 누른사용자중에 로그인한유저의 아이디가 있다면 
-    if(joinid.match(id)){ 
-		$("#joinBtn").css("display", "none");
-		var $div = $('<input class="btn btn-danger cancel" style="width: 40%" type="button"  value="취소하기"/>');
-		
-		$("#buttonarea").append($div);
-		test();
-	} 
 	
     //참가하기 버튼을 눌렀을때 
 	$("#joinBtn").click(function(){
 		
-	        $.ajax({
-	        		type:'post',
-	        		async:true,
-	                url : 'mealjoin.do',
-	                contentType :'application/x-www-form-urlencoded;charset=UTF-8',
-	                data :
-	            		"f_userId=" + joinuser+","+userId+
-	            		"&b_no="+ b_no,
-	            	dataType:"text",
-	                success : function(resultData){
-	                	alert("밥친구로 확정되었습니다.");
-	                	//넘겨받은 결과값이 0이 아니라면 
-	                	if(resultData != 0){
-	                		//참가하기 버튼 숨기고
-	                		$("#joinBtn").css("display", "none");
-	                		//취소하기버튼 띄워주기 
-	                		var $div = $('<button class="btn btn-danger cancel" style="width: 40%" type="button">취소하기</button>');
-	                		$("#buttonarea").append($div);
-	                		test();
+	      alert("밥친구로 확정되었습니다.");
+	      //참가하기 버튼 숨기고
+	      $("#joinBtn").css("display", "none");
+	      //취소하기버튼 띄워주기 
+	      var $div = $('<button class="btn btn-danger cancel" style="width: 40%" type="button">취소하기</button>');
+	      $("#buttonarea").append($div);
+	      test();
 	                		
-	                	}else{
-	                		
-	                	}
-	                	
-	                	
-	                },
-	        		error:function(xhr, ajaxOptions, thrownError){
-	        			alert("에러"+xhr.status);
-	        			alert("에러"+thrownError);
-	        			console.log(e);
-	        		}
-	        });
 		});
 	
 	function test(){
 		$(".cancel").off('click');
 		$(".cancel").on('click',function(){
-			var joinid = $("#f_userId").val()
-			var userid = $("#chat_id").val()
 			
-		    $.ajax({
-		    	type:'post',
-				async:true,
-				
-		        url : 'cancel.do',
-		        contentType :'application/x-www-form-urlencoded;charset=UTF-8',
-		        data :
-		    		"f_userId=" + $("#f_userId").val()+"&userId="+$("#chat_id").val()+
-		    		"&b_no="+ $("#b_no").val()+"&f_cnt="+$("#f_cnt").val(),
-		    	//dataType:"text",
-		        success : function(resultData){
-		        	//넘겨받은 결과값이 0이 아니라면 
-		        	if(resultData != 0){
-		        		//취소하기 버튼 숨기고
-		        		alert("밥친구가 취소되었습니다.");
-		        		$(".cancel").css("display", "none");
-		        		$("#f_userId").remove()
-		       		//참가하기 버튼 띄워주기
-		        		var $div = $('<button class="btn btn-info" style="width: 40%" type="button" id="joinBtn">밥친구확정 </button>');
-		        		$("#buttonarea").append($div);
+		        alert("밥친구가 취소되었습니다.");
+		        $(".cancel").css("display", "none");
+		       	//참가하기 버튼 띄워주기
+		        var $div = $('<button class="btn btn-info" style="width: 40%" type="button" id="joinBtn">밥친구확정 </button>');
+		        $("#buttonarea").append($div);
+		        test2();
 		
-		        	}else{
-		        		alert("밥친구가 아닙니다.");
-		        	}
-		            	
-		            	
-		            },
-		    		error:function(xhr, ajaxOptions, thrownError){
-		    			alert("에러"+xhr.status);
-		    			alert("에러"+thrownError);
-		    			console.log(e);
-		    		}
-		    });
 		});
 	}
+	
+	
+	function test2(){
+		$("#joinBtn").off('click');
+		$("#joinBtn").on('click',function(){
+			
+			  alert("밥친구로 확정되었습니다.");
+		      //참가하기 버튼 숨기고
+		      $("#joinBtn").css("display", "none");
+		      //취소하기버튼 띄워주기 
+		      var $div = $('<button class="btn btn-danger cancel" style="width: 40%" type="button">취소하기</button>');
+		      $("#buttonarea").append($div);
+		      test();
+		
+		});
+	}
+	
 	
     
     //메세지를 보내기위한 함수
